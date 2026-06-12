f21ab77df980a98f384f1940358f5263cd764b58
Ken Zhu <89586361+justkuhz@users.noreply.github.com>
2026-06-11 18:57:45 -0700

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -0,0 +1,38 @@
+"""
+constraints:
+how long can the nums list be?
+at most 1000 elements in list
+
+is there a possibility of no solutions or empty list?
+one valid answer exists in every case (never empty list)
+
+how should i handle if there are multiple solutions?
+answered above
+
+approach:
+we can solve this in one pass and use a hashmap to keep track of previous values we've seen and
+the index we saw them at. As we see new values we will evaluate whether we meet the two sum
+condition or not.
+
+One pass = O(n) time
+hash map = O(n) space
+
+post problem thoughts:
+
+"""
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # init hash map and res
+        seen = {}
+
+        # iterate over nums
+        for i in range(len(nums)):
+            # check if valid complement in hashmap
+            complement = target - nums[i]
+            if (complement) in seen:
+                return [seen[complement], i]
+
+            # insert current value/index into hashmap
+            seen[nums[i]] = i
+
+        
