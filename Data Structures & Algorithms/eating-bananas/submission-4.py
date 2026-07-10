7146d34d2082ecae0de8a7fa837082f953e47232
Ken Zhu <89586361+justkuhz@users.noreply.github.com>
2026-07-09 19:29:46 -0700

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,34 @@
+'''
+sorting is not super useful since each time we pick an hour k we have to calculate total
+time it takes to clear all piles
+
+k will always be bound between 1 (slowest) and max(piles)
+
+time complexity would be O(nlogn) and space is O(1) 
+
+'''
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        lo = 1
+        hi = max(piles)
+        res = hi
+
+        def time_to_finish(rate: int, piles: List[int]) -> int:
+            time = 0
+            # round up
+            for b in piles:
+                time += -(-b // rate)
+            return time
+
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            time = time_to_finish(mid, piles)
+
+            if time <= h:
+                res = mid
+                hi = mid
+            else:
+                lo = mid + 1
+
+        return res
+        
